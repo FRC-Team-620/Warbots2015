31b9bd712d745b4d20eec5e10d70b1279c79f514
David Bell <thedsmileyball@gmail.com>
2015-02-15 11:24:55 -0500

    Pullofftwocan
    Same fixes and testing needed as last commit.

--- src/org/usfirst/frc620/Warbots2015/commands/PullOffTwoCans.java
+++ src/org/usfirst/frc620/Warbots2015/commands/PullOffTwoCans.java
@@ -34,5 +34,17 @@ public class PullOffTwoCans extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
+    	addSequential(new Drive(1,0,.5,0));
+    	addSequential(new extend());
+    	addSequential(new movePicker());
+    	addSequential(new retract());
+    	addSequential(new Drive(1,0,0,-1)); //Turn 180 degrees
+    	addSequential(new movePicker());
+    	addSequential(new Drive(1,.5,0,-1)); //Turn 180 degrees while moving to next can
+    	addSequential(new extend());
+    	addSequential(new movePicker());
+    	addSequential(new retract());
+    	addSequential(new Drive(1,-.5,0,0));
+    	addSequential(new Drive(1,0,-.5,0));
     }
 }
